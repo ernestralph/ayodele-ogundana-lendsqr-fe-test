b0c1c7a0eac810721e219a8c97b542d27785b559
Ogundana Ayodele Raphael <82033398+ernestralph@users.noreply.github.com>
2022-11-06 23:59:03 +0100

    ....

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-app.use(express.static('reactFolderName/build'));
+// app.use(express.static('reactFolderName/build'));
 const publicPath = path.join(__dirname, '..', 'public');
 const port = process.env.PORT || 3000;
 
